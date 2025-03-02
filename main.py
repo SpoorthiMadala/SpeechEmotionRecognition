51a0da336528fa14a025696a7fac09a999d8487c
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2025-03-02 13:13:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import os
 
-print("🚀 Starting Speech Emotion Recognition Project...\n")
+print("Starting Speech Emotion Recognition Project...\n")
 
 # Step 1: Extract Features
 print("📂 Step 1: Extracting Features...")
 if os.system("python scripts/data_preprocessing.py") == 0:
-    print("✅ Feature Extraction Completed.\n")
+    print("Feature Extraction Completed.\n")
 else:
-    print("❌ Feature Extraction Failed. Check for errors!")
+    print("Feature Extraction Failed. Check for errors!")
     exit()
 
 # Step 2: Train Model
-print("🎯 Step 2: Training Model...")
+print("Step 2: Training Model...")
 if os.system("python scripts/train_model.py") == 0:
-    print("✅ Model Training Completed.\n")
+    print("Model Training Completed.\n")
 else:
-    print("❌ Model Training Failed. Check for errors!")
+    print("Model Training Failed. Check for errors!")
     exit()
 
 # Step 3: Evaluate Model
-print("📊 Step 3: Evaluating Model...")
+print("Step 3: Evaluating Model...")
 if os.system("python scripts/evaluate_model.py") == 0:
-    print("✅ Model Evaluation Completed.\n")
+    print("Model Evaluation Completed.\n")
 else:
-    print("❌ Model Evaluation Failed. Check for errors!")
+    print("Model Evaluation Failed. Check for errors!")
     exit()
 
-print("📊 Step 4: Predicting Model...")
+print("Step 4: Predicting Model...")
 if os.system("python scripts/predict_emotion.py") == 0:
-    print("✅ Model Prediction Completed.\n")
+    print("Model Prediction Completed.\n")
 else:
-    print("❌ Model Prediction Failed. Check for errors!")
+    print("Model Prediction Failed. Check for errors!")
     exit()
 
 
-print("🎉 SER Project Completed Successfully! 🚀")
+print("SER Project Completed Successfully!")
 
